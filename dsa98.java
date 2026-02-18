d2a0c24b9789160fe2083df0addbf5655608c9c6
Roshan1772006 <roshankaduskar6@gmail.com>
2026-02-18 17:36:57 +0530

    Add files via upload

---
+++ dsa98.java
@@ -0,0 +1,17 @@
+
+
+public class dsa98 {
+ public static void main(String[] args) {
+    int [] arr =  {6,16,98,4,56};
+    System.out.println(arr[3]);
+    System.out.println(arr[2]);
+    System.out.println(arr[1]);
+    System.out.println(arr[0]);
+    arr[1] = 9;
+    System.out.println(arr[1]);
+     
+    int[] x = new int[5];
+    x[1]= 1;
+    System.out.println(x[1]);
+ }   
+}
---
+++ dsa99.java
@@ -0,0 +1,8 @@
+public class dsa99 {
+    public static void main(String[] args) {
+        int[] arr = {5,8,9,6,2,7,3,1};
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");           
+        }
+    }
+}
