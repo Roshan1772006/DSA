7b4fd8f99dc3899630847c9b96fe146d480f9f68
Roshan1772006 <roshankaduskar6@gmail.com>
2026-03-05 20:21:58 +0530

    Add files via upload

---
+++ dsa130.java
@@ -0,0 +1,38 @@
+
+class dsa130 {
+    void sort(int a[])
+    {
+        int n = a.length;
+
+       
+        for (int i = 0; i < n - 1; i++) {
+          
+           
+            int min_idx = i;
+          
+            for (int j = i + 1; j < n; j++) {
+                if (a[j] < a[min_idx])
+                    min_idx = j;
+            }
+
+           
+            int temp = a[min_idx];
+            a[min_idx] = a[i];
+            a[i] = temp;
+        }
+    }
+
+   
+    public static void main(String args[])
+    {
+        dsa130 ob = new dsa130();
+        int a[] = { 64, 25, 12, 22, 11 };
+
+        ob.sort(a);
+      	
+      	int n = a.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(a[i] + " ");
+
+    }
+}
